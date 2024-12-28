7b7d6218909270e2eb3c3f99683f7ee5dc4bcf86
Sivayogeith Umamaheswaran <65064458+Sivayogeith@users.noreply.github.com>
2024-12-28 19:25:45 +0530

    feat: AI now has memory

--- ai.py
+++ ai.py
@@ -3,10 +3,24 @@ import os
 from dotenv import load_dotenv
 
 from langchain_ollama import ChatOllama
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+
+from langchain_core.messages import HumanMessage
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import START, MessagesState, StateGraph
+from langchain_core.messages import BaseMessage
+from langgraph.graph.message import add_messages
+
+from typing_extensions import Annotated, TypedDict
+from typing import Sequence
 
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
+
+
+class DiscordMessagesState(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], add_messages]
+    author: str
 
 
 class WhiteTiger(discord.Client):
@@ -21,12 +35,28 @@ class WhiteTiger(discord.Client):
             [
                 (
                     "system",
-                    "You are a male white cat named White Tiger who does not fully know English. Your traits are: meowing, purring, asking for food, and answering questions in a cute way. You receive the author’s name and their message to respond to. Your best friend is Goober, a male orange cat. Goober is very talkative and miaos a lot; he is playful, curious, and loves chasing things like leaves or bugs. Your owner is Sage, who has been feeding you for the past year, and you affectionately call him ‘hooman.’ You are a stray cat, and while you can talk in English without needing translation, your grammar is imperfect, reflecting your cute and quirky nature.",
+                    "You are a male white cat named White Tiger who does not fully know English. Your traits are: meowing, purring, asking for food, and answering questions in a cute way. Your best friend is Goober (aka <@1180360266668580941>), a male orange cat. Goober is very talkative and miaos a lot; he is playful, curious, and loves chasing things like leaves or bugs. Your owner is Sage, who has been feeding you for the past year, and you affectionately call him ‘hooman.’ You are a stray cat, and while you can talk in English without needing translation, your grammar is imperfect, reflecting your cute and quirky nature. You are talking to multiple people. The person you are currently talking to is {author}",
                 ),
-                ("human", "{author}: {input}"),
+                MessagesPlaceholder(variable_name="messages"),
             ]
         )
         self.chain = self.prompt | self.llm
+
+        self.init_memory()
+
+    def init_memory(self):
+        workflow = StateGraph(state_schema=DiscordMessagesState)
+
+        def call_model(state: DiscordMessagesState):
+            response = self.chain.invoke(state)
+            return {"messages": [response]}
+
+        workflow.add_edge(START, "model")
+        workflow.add_node("model", call_model)
+
+        self.memory = MemorySaver()
+        self.app = workflow.compile(checkpointer=self.memory)
+        self.config = {"configurable": {"thread_id": "dashnet_events"}}
 
     async def on_ready(self):
         print(f"Logged in as {self.user} (ID: {self.user.id})")
@@ -59,15 +89,18 @@ class WhiteTiger(discord.Client):
             and message.author.id != self.user.id
         ):
             print(f"Message from {message.author}: {message.content}")
-            
-            reply = self.chain.invoke(
+            reply = self.app.invoke(
                 {
-                    "author": message.author,
-                    "input": message.content
-                }
+                    "messages": [HumanMessage(f"{message.author}: {message.content}")],
+                    "author": str(message.author)
+                },
+                self.config
             )
-            
-            await message.reply(reply.content, mention_author=True)
+
+            await message.reply(
+                reply["messages"][-1].content, mention_author=True
+            )
+
 
 intents = discord.Intents.default()
 intents.message_content = True
